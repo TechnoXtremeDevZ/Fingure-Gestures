51ec03233292dfec9c83cfe24f7f1f052521cbab
TechnoXtremeDevZ <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2022-01-26 13:31:38 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+﻿/* The ratio of the Width & Height in Webcam.set({}) should
+always be 4:3 */
+
+Webcam.set({
+    width: 400,
+    height: 300,
+    image_format: "png",
+    image_quality: 90
+})
+
+camera = document.getElementById("box");
+Webcam.attach(camera);
+
+function start() {
+    Webcam.start(function (data_uri) {
+        document.getElementById("output").innerHTML =
+            "<img id='selfie' src=" + data_uri + "></img>" 
+    })
+}
+
+console.log("ml5", ml5.version);
+var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/ZI2tRkTYP/", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model is Loaded");
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_ = "My prediction is " + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_);
+    synth.speak(utterThis);
+}
