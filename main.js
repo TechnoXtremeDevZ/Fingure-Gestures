62ccf0ffb42edcb2aeb6dee5e7a9fd19d1c479b9
TechnoXtremeDevZ <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2022-02-08 17:57:45 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-﻿/* The ratio of the Width & Height in Webcam.set({}) should
+/* The ratio of the Width & Height in Webcam.set({}) should
 always be 4:3 */
 
 Webcam.set({
@@ -12,14 +12,14 @@ camera = document.getElementById("box");
 Webcam.attach(camera);
 
 function start() {
-    Webcam.start(function (data_uri) {
+    Webcam.snap(function (data_uri) {
         document.getElementById("output").innerHTML =
-            "<img id='selfie' src=" + data_uri + "></img>" 
+            "<img id='selfie' src=" + data_uri + "></img>"
     })
 }
 
 console.log("ml5", ml5.version);
-var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/ZI2tRkTYP/", modelLoaded);
+var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/ZI2tRkTYP/model.json", modelLoaded);
 
 function modelLoaded() {
     console.log("Model is Loaded");
@@ -30,4 +30,40 @@ function speak() {
     speak_ = "My prediction is " + prediction;
     var utterThis = new SpeechSynthesisUtterance(speak_);
     synth.speak(utterThis);
+}
+
+function end() {
+    var img = document.getElementById("selfie");
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        prediction = results[0].label;
+
+        document.getElementById("fingure-gesture-1").innerHTML = prediction;
+
+        if (prediction == "Thumbs Up") {
+            document.getElementById("fingure-gesture-2").innerHTML = "👍";
+        }
+        else if (prediction == "Thumbs Down") {
+            document.getElementById("fingure-gesture-2").innerHTML = "👎";
+        }
+        else if (prediction == "Nice") {
+            document.getElementById("fingure-gesture-2").innerHTML = "👌";
+        }
+        else if (prediction == "Peace") {
+            document.getElementById("fingure-gesture-2").innerHTML = "🤘";
+        }
+        else if (prediction == "Gun") {
+            document.getElementById("fingure-gesture-2").innerHTML = "☞";
+        }
+        else if (prediction == "Pistol") {
+            document.getElementById("fingure-gesture-2").innerHTML = "☞";
+        }
+    }
 }
